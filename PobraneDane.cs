d593bb97d044509a3f038d832ba594db575bf9fe
PawelPawlowski96 <bolec0.17@gmail.com>
2016-12-12 17:46:39 +0100

    Dodanie nowej klasy i pierwszych metod

---
+++ PobraneDane.cs
@@ -0,0 +1,43 @@
+﻿namespace CsgoSkinPriceCheck
+
+{
+ 
+   public class PobraneDane
+   
+ {
+        
+public string nazwaBroni 
+{ get; set; 
+}
+        
+public string nazwaSkina 
+{
+ get;
+ set;
+ }
+        
+public string cenaSkina 
+{ 
+get; 
+set; 
+}
+        
+public string cenaStatTrak 
+{
+ get;
+ set; 
+}
+        
+public string sciezkaDoGrafiki 
+{ get; 
+set; 
+}
+ 
+ public int numerLiniiNazwySkina 
+{ get;
+ set;
+ }
+
+}
+
+}
